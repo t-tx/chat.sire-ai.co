4d4cb3ed26794c120aa58735acbc41cba756b1cc
t-tx <trungtvq.work@gmail.com>
2024-12-22 22:10:00 +0700

    fix nextjs export setting

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   trailingSlash: true,
+  images: {
+    unoptimized: true, // Optional: Disables image optimization (useful when exporting)
+  },
+  exportTrailingSlash: true,  // Optional: Adds a trailing slash to the URLs
 };
 
 export default nextConfig;
